b983cb7f1745309abe996e162224eea7f48a36cc
PedroH-CElias <phenriqueelias29@gmail.com>
2025-08-24 21:52:45 -0300

    Paymente entity created

---
+++ src/main/java/com/devsuperior/dscommerce/entities/Payment.java
@@ -0,0 +1,75 @@
+package com.devsuperior.dscommerce.entities;
+
+import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_payment")   
+public class Payment {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	/**
+	 * Salva no banco um instante no tempo como sendo UTC
+	 */
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant moment;
+	
+	/**
+	 * Nesse caso o Payment é dependente de um Order (pedido)
+	 * @MapsId representa a chave estrangeira.
+	 * Este atributo será mapeado como a chave primária da entidade "filha" e 
+	 * será o mesmo valor da chave primária da entidade relacionada.
+	 * 
+	 * A chave primaria do pagamento vai ser uma chave estrangeira referente ao id de um pedido
+	 */
+	@OneToOne
+	@MapsId
+	private Order order;
+	
+	public Payment() {
+		
+	}
+
+	public Payment(Long id, Instant moment, Order order) {
+		this.id = id;
+		this.moment = moment;
+		this.order = order;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instant getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+	
+	
+}
